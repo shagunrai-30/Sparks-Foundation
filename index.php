532daaffddc636a0d21d8f19a1855388e29bd605
shagunrai-30 <84097046+shagunrai-30@users.noreply.github.com>
2021-07-15 00:37:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <title>THE TSF BANK</title>
 
