37bc482148254beccbe3833a4b56f5a2ece149df
shagunrai-30 <84097046+shagunrai-30@users.noreply.github.com>
2021-07-15 00:13:05 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <title>THE TSF BANK</title>
+
+    <body>
+        <ul>
+            <li><img src="imgs/tsf.png" id="logo"></li>
+            <li><a href="index.php">Home</a></li>
+            <li class="dropdown">
+              <a href="javascript:void(0)" class="dropbtn">Action</a>
+              <div class="dropdown-content">
+                <a href="transfermoney.php">Transfer Money</a>
+                <a href="History.php">Transaction History</a>
+              </div>
+            </li>
+            <li><a href="https://www.thesparksfoundationsingapore.org/">About US</a></li>
+            <li><a href="https://www.thesparksfoundationsingapore.org/contact-us/">Contact US</a></li>
+          </ul>
+          <pre>
+
+
+
+
+          </pre>
+          <section id="first">
+            <h1> This is my internship task: BASIC BANKING SYSTEM.</h1>
+            <h2>By: SHAGUN RAI</h2>
+          </section>
+          <pre>
+
+
+
+
+
+
+
+
+
+
+
+
+          </pre>
+          <footer>
+            <h3>The Sparks Foundation</h3>
+          </footer>
+    </body>
+</html>
